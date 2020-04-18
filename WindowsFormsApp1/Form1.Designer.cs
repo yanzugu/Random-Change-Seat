462e2f071ad2037d5b9342f4e2f50013febc1587
yanzugu <yanzugu@gmail.com>
2020-04-19 04:26:07 +0800

    加入專案檔案。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RestartButton = new System.Windows.Forms.Button();
+            this.NumberListBox = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.ConfirmButton = new System.Windows.Forms.Button();
+            this.ColumnBox = new System.Windows.Forms.TextBox();
+            this.RowBox = new System.Windows.Forms.TextBox();
+            this.StudentBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // RestartButton
+            // 
+            this.RestartButton.Location = new System.Drawing.Point(1109, 938);
+            this.RestartButton.Name = "RestartButton";
+            this.RestartButton.Size = new System.Drawing.Size(75, 23);
+            this.RestartButton.TabIndex = 0;
+            this.RestartButton.Text = "Restart";
+            this.RestartButton.UseVisualStyleBackColor = true;
+            this.RestartButton.Click += new System.EventHandler(this.RestartButton_Click);
+            // 
+            // NumberListBox
+            // 
+            this.NumberListBox.FormattingEnabled = true;
+            this.NumberListBox.Location = new System.Drawing.Point(676, 114);
+            this.NumberListBox.Name = "NumberListBox";
+            this.NumberListBox.Size = new System.Drawing.Size(120, 140);
+            this.NumberListBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(389, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "黑版";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(274, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "人數 : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(482, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "列";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(252, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 19);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "排";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(374, 248);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "X";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(599, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "參與號碼";
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(104, 2);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 8;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(1, 2);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 9;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // ConfirmButton
+            // 
+            this.ConfirmButton.Location = new System.Drawing.Point(347, 394);
+            this.ConfirmButton.Name = "ConfirmButton";
+            this.ConfirmButton.Size = new System.Drawing.Size(75, 23);
+            this.ConfirmButton.TabIndex = 10;
+            this.ConfirmButton.Text = "Confirm";
+            this.ConfirmButton.UseVisualStyleBackColor = true;
+            this.ConfirmButton.Click += new System.EventHandler(this.ConfirmButton_Click);
+            // 
+            // ColumnBox
+            // 
+            this.ColumnBox.Location = new System.Drawing.Point(236, 245);
+            this.ColumnBox.Name = "ColumnBox";
+            this.ColumnBox.Size = new System.Drawing.Size(100, 22);
+            this.ColumnBox.TabIndex = 11;
+            this.ColumnBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ColumnBox_KeyPress);
+            // 
+            // RowBox
+            // 
+            this.RowBox.Location = new System.Drawing.Point(448, 245);
+            this.RowBox.Name = "RowBox";
+            this.RowBox.Size = new System.Drawing.Size(100, 22);
+            this.RowBox.TabIndex = 12;
+            this.RowBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.RowBox_KeyPress);
+            // 
+            // StudentBox
+            // 
+            this.StudentBox.Location = new System.Drawing.Point(342, 165);
+            this.StudentBox.Name = "StudentBox";
+            this.StudentBox.Size = new System.Drawing.Size(100, 22);
+            this.StudentBox.TabIndex = 13;
+            this.StudentBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.StudentBox_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 961);
+            this.Controls.Add(this.StudentBox);
+            this.Controls.Add(this.RowBox);
+            this.Controls.Add(this.ColumnBox);
+            this.Controls.Add(this.ConfirmButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NumberListBox);
+            this.Controls.Add(this.RestartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RestartButton;
+        private System.Windows.Forms.CheckedListBox NumberListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button ConfirmButton;
+        private System.Windows.Forms.TextBox ColumnBox;
+        private System.Windows.Forms.TextBox RowBox;
+        private System.Windows.Forms.TextBox StudentBox;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int row = 0, col = 0, students = 0;
+        public bool[,] state;
+        List<int> li;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            width = this.Width;
+            height = this.Height;
+            label1.Location = new Point(this.width / 2 - label1.Width / 2, 5);
+            label1.Visible = false;
+            StudentBox.Location = new Point(this.width / 2 - ColumnBox.Width / 2, 250);
+            ColumnBox.Location = new Point(this.width / 2 - RowBox.Width / 2 - 65, 380);
+            RowBox.Location = new Point(this.width / 2 - StudentBox.Width / 2 + 65, 380);
+            ConfirmButton.Location = new Point(this.width / 2 - RestartButton.Width / 2, 500);
+            label2.Location = new Point(this.width / 2 - label2.Width / 2 - 90, 250);
+            label3.Location = new Point(this.width / 2 - label3.Width / 2 + 55, 420);
+            label4.Location = new Point(this.width / 2 - label4.Width / 2 - 55, 420);
+            label5.Location = new Point(this.width / 2 - label5.Width / 2, 390);
+            label6.Location = new Point(this.width / 2 + 80, 260);
+            NumberListBox.Location = new Point(this.width / 2 + 150, 250);
+            label6.Visible = false;
+            NumberListBox.Visible = false;
+            RowBox.Enabled = false;
+            ColumnBox.Enabled = false;
+            ResetButton.Visible = false;
+            StartButton.Visible = false;
+        }
+
+        int btnval = 0;
+        public int width, height, size, totalTable;
+
+        private void StudentBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < '0' || e.KeyChar > '9') && e.KeyChar != '\b')
+                e.Handled = true;
+        }
+
+        private void ColumnBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < '0' || e.KeyChar > '9') && e.KeyChar != '\b')
+                e.Handled = true;
+        }
+
+        private void RowBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < '0' || e.KeyChar > '9') && e.KeyChar != '\b')
+                e.Handled = true;
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            for (int r = 0; r < row; r++)
+                for (int c = 0; c < col; c++)
+                {
+                    buttons[r, c].Enabled = true;
+                    buttons[r, c].Text = "O";
+                    state[r, c] = true;
+                    totalTable = row * col;
+                }
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            int idx = 0;
+            for (int i = 0; i < students * 100; i++)
+            {
+                int tmp = rd.Next() % students;
+                int tmp2 = rd.Next() % students;
+                int tmp3 = li[tmp];
+                li[tmp] = li[tmp2];
+                li[tmp2] = tmp3;
+            }
+
+            for (int r = 0; r < row; r++)
+                for (int c = 0; c < col; c++)
+                {
+                    buttons[r, c].Enabled = false;
+                    if (state[r, c] == true && idx < students)
+                    {
+                        buttons[r, c].Text = li[idx].ToString();
+                        idx++;
+                    }
+                }
+        }
+
+        private void RestartButton_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void ConfirmButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (btnval == 0)
+                {
+                    students = int.Parse(StudentBox.Text);
+                    if (students == 0)
+                    {
+                        MessageBox.Show("班上一個人都沒有?");
+                        return;
+                    }
+                    if (students > 100)
+                    {
+                        MessageBox.Show("班上人太多了歐!");
+                        return;
+                    }
+                    StudentBox.Enabled = false;
+                    ColumnBox.Enabled = true;
+                    RowBox.Enabled = true;
+                    label6.Visible = true;
+                    NumberListBox.Visible = true;
+                    btnval++;
+                    for (int i = 0; i < students; i++)
+                    {
+                        NumberListBox.Items.Add("" + (i + 1));
+                        NumberListBox.SetItemChecked(i, true);
+                    }
+                }
+                else
+                {
+                    col = int.Parse(ColumnBox.Text);
+                    row = int.Parse(RowBox.Text);
+                    totalTable = row * col;
+                    if (totalTable == 0)
+                    {
+                        MessageBox.Show("請輸入正確數值");
+                        return;
+                    }
+                    if (totalTable > 100)
+                    {
+                        MessageBox.Show("全校桌椅都在你們班?");
+                        return;
+                    }
+                    if ((row > 3 * col || col > 3 * row) && totalTable > 60)
+                    {
+                        MessageBox.Show("請輸入合理的規格!");
+                        return;
+                    }
+                    state = new bool[row, col];
+                    for (int r = 0; r < row; r++)
+                        for (int c = 0; c < col; c++)
+                            state[r, c] = true;
+                    li = new List<int>();
+                    for (int i = 0; i < students; i++)
+                        if (NumberListBox.GetItemChecked(i))
+                            li.Add(i + 1);
+
+                    students = li.Count;
+                    if (row * col < students)
+                    {
+                        MessageBox.Show("位置太少啦!!");
+                        return;
+                    }
+                    NumberListBox.Visible = false;
+                    label6.Visible = false;
+                    ConfirmButton.Location = new Point(ConfirmButton.Left + 180, ConfirmButton.Top);
+                    changeScr();
+                    generate_Button();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("請輸入正確數值");
+            }
+        }
+
+
+        private void changeScr()
+        {
+            label1.Visible = true;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            ColumnBox.Visible = false;
+            RowBox.Visible = false;
+            StudentBox.Visible = false;
+            ConfirmButton.Visible = false;
+            ResetButton.Visible = true;
+            StartButton.Visible = true;
+        }
+        Button[,] buttons;
+        private void generate_Button()
+        {
+            width /= (col + 1);
+            height = (height - 40) / (row + 1);
+            size = width > height ? height : width;
+            buttons = new Button[row, col];
+            for (int r = 0; r < row; r++)
+            {
+                for (int c = 0; c < col; c++)
+                {
+                    buttons[r, c] = new Button();
+                    buttons[r, c].Location = new Point(width * (c + 1) - size / 2, height * (r + 1) - 40);
+                    buttons[r, c].Size = new Size(size, size);
+                    buttons[r, c].Text = "O";
+                    Font f = buttons[r, c].Font;
+                    buttons[r, c].Font = new Font(f.FontFamily, size / 3);
+                    buttons[r, c].Name = "" + r + ',' + c;
+                    buttons[r, c].Click += new EventHandler(this.buttons_Click);
+                    this.Controls.Add(buttons[r, c]);
+                }
+            }
+        }
+
+        private void buttons_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            string[] tmp = btn.Name.Split(',');
+            int r = int.Parse(tmp[0]);
+            int c = int.Parse(tmp[1]);
+            if (state[r, c] == true)
+            {
+                if (totalTable <= students)
+                {
+                    MessageBox.Show("位置不夠啦!!");
+                    return;
+                }
+                btn.Text = "X";
+                totalTable--;
+                state[r, c] = !state[r, c];
+            }
+            else
+            {
+                btn.Text = "O";
+                totalTable++;
+                state[r, c] = !state[r, c];
+            }
+        }
+    }
+}
